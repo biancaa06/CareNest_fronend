3ffbedb753d90a2234bc16c7921759e1ca08a4a0
mariucaBianca <540269@student.fontys.nl>
2024-09-30 22:34:28 +0200

    added logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,18 +4,15 @@ import '../css/navbar.css';
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-green-600 text-white fixed top-0 left-0 w-full shadow-lg">
-      <div className="container-fluid">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      <div className="container-fluid flex justify-between items-center">
+        <a href="/" className="navbar-brand">
+          <img
+            src="/images/carenest_logo.png"
+            alt="CareNest Logo"
+            className="navbar-logo"
+          />
+        </a>
+
         <div className="collapse navbar-collapse justify-center" id="navbarNav">
           <ul className="navbar-nav gap-4">
             <li className="nav-item">
@@ -38,12 +35,13 @@ const Navbar = () => {
                 <i className="fas fa-user-nurse"></i> Caretakers
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link text-white font-bold" href="/login">
-                <i className="fas fa-sign-in-alt"></i> Login
-              </a>
-            </li>
           </ul>
+        </div>
+
+        <div className="navbar-login">
+          <a className="nav-link text-white font-bold" href="/login">
+            <i className="fas fa-sign-in-alt"></i> Login
+          </a>
         </div>
       </div>
     </nav>
