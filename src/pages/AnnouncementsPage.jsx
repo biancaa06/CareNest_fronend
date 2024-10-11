d6a229c066a7d4d66d366ab61af61d1ffd5b0cf0
mariucaBianca <540269@student.fontys.nl>
2024-10-11 15:19:29 +0200

    renamed some folders

--- src/pages/AnnouncementsPage.jsx
+++ src/pages/AnnouncementsPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import AnnouncementList from "../components/Announcements/AnnouncementList";
+import AnnouncementList from "../components/AnnouncementsPage/AnnouncementList";
 import '../css/AnnouncementsPage.css';
 import { getAllAnnouncements } from "../services/AnnouncementsRepository";
 
