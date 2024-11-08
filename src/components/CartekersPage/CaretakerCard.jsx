91042affe3f09a01158758bb7b157152d3b2b177
mariucaBianca <540269@student.fontys.nl>
2024-11-08 16:36:34 +0100

    see real picture everywhere

--- src/components/CartekersPage/CaretakerCard.jsx
+++ src/components/CartekersPage/CaretakerCard.jsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from "react";
+
 function CaretakerCard({ caretaker }) {
+
+    const [profilePicture, setProfilePicture] = useState(caretaker.profileImage);
+
+    useEffect(() => {
+        if (caretaker?.baseUser?.profileImage) {
+            const base64Image = `data:image/jpeg;base64,${caretaker.baseUser.profileImage}`;
+            setProfilePicture(base64Image);
+        } else {
+            setProfilePicture(null);
+        }
+    }, [caretaker]);
+
     return (
         <div className="w-[500px] max-w-md bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-auto">
             <div className="flex justify-end px-4 pt-4">
@@ -7,11 +21,17 @@ function CaretakerCard({ caretaker }) {
             </div>
 
             <div className="flex flex-col items-center pb-10">
-                <img
-                    className="w-24 h-24 mb-3 rounded-full shadow-lg"
-                    src={caretaker.baseUser.profileImage || "./images/default_profile.png"}
-                    alt="Profile"
-                />
+                {profilePicture ? (
+                    <img
+                        src={profilePicture}
+                        alt="Profile"
+                        className="w-24 h-24 rounded-full object-cover"
+                    />
+                ) : (
+                    <div className="w-24 h-24 bg-green-100 rounded-full flex items-center justify-center text-3xl font-semibold text-green-700">
+                        {caretaker.baseUser.firstName?.[0]}{caretaker.baseUser.lastName?.[0]}
+                    </div>
+                )}
                 <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
                     {caretaker.baseUser.firstName} {caretaker.baseUser.lastName}
                 </h5>
