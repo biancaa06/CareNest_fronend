30e703ec9f702b6c2f7e1bde032db0162af9703e
mariucaBianca <540269@student.fontys.nl>
2024-11-19 21:11:21 +0100

    profile on navbar after login
    you can still see everyone's profile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -24,13 +24,21 @@ const Navbar = ({ claims, onLogout }) => {
           <Link to="/caretakers" className="text-white font-bold hover:bg-green-700 px-4 py-2 rounded">
             <i className="fas fa-user-nurse"></i> Caretakers
           </Link>
-
           {claims?.roles?.includes('MANAGER') && (
             <Link to="/managersManagement" className="text-white font-bold hover:bg-green-700 px-4 py-2 rounded">
               <i className="fas fa-user-cog"></i> Managers Management
             </Link>
           )}
         </div>
+
+        {claims?.userId &&(
+            <Link to={`/profile/${claims.userId}`} 
+                className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" 
+                id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+              <img className="w-8 h-8 rounded-full" src="/public/images/default_profile.png" alt="user photo" />
+            </Link>
+          )
+        }
 
         {claims ? (
           <Logout onLogout={onLogout} />
